6d1baaecedbfa1b290e8b1f1515a5427b9a331f7
Cleicião Diego Moro <92747697+cleiciao@users.noreply.github.com>
2024-08-03 18:50:21 -0300

    Update rpz-block.sh

--- rpz-block.sh
+++ rpz-block.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # URL do script Python hospedado
-SCRIPT_URL=https://raw.githubusercontent.com/cleiciao/dns/main/block.py"
+SCRIPT_URL="https://raw.githubusercontent.com/user/repo/branch/rpz_script.py"
 
 # Caminho para salvar o script baixado
-SCRIPT_PATH="/etc/unbound/block.py"
+SCRIPT_PATH="/tmp/rpz_script.py"
 
 # Baixar o script usando curl
 echo "Baixando o script de RPZ..."
@@ -12,7 +12,7 @@ curl -o $SCRIPT_PATH $SCRIPT_URL
 
 # Verificar se o download foi bem-sucedido
 if [ $? -ne 0 ]; then
-  #echo "Falha ao baixar o script. Verifique a URL e a conexão de rede."
+  echo "Falha ao baixar o script. Verifique a URL e a conexão de rede."
   exit 1
 fi
 
@@ -26,14 +26,19 @@ python3 $SCRIPT_PATH
 
 # Verificar se a execução foi bem-sucedida
 if [ $? -ne 0 ]; then
- echo "Falha ao executar o script. Verifique o script Python para erros."
+  echo "Falha ao executar o script. Verifique o script Python para erros."
   exit 1
 fi
 
-#echo "Script executado com sucesso."
+echo "Script executado com sucesso."
 
 # Adicionar agendamento ao crontab
 echo "Agendando a execução diária do script no crontab..."
-CRON_JOB="0 3 * * * python3 $SCRIPT_PATH"
-(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+(crontab -l ; echo "0 3 * * * python3 $SCRIPT_PATH") | crontab -
 
+if [ $? -eq 0 ]; then
+  echo "Script agendado com sucesso para ser executado diariamente às 3:00 AM."
+else
+  echo "Falha ao agendar o script. Verifique as permissões do crontab."
+  exit 1
+fi
